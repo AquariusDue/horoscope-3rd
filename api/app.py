65b798829067259a57cd9ee71d11c79881f8cd48
Mihai Dumitru <mihai14x7@gmail.com>
2021-02-22 00:44:28 +0200

    Open horoscopes.json and save the data as in-memory dict when app starts

--- api/app.py
+++ api/app.py
@@ -1,7 +1,14 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
+# TODO: Data is stored in memory, once the file is updated by the scraper the in-memory api data falls behind
+# try route middleware?
+with open('../horoscopes.json', 'r') as f:
+    horoscopes = json.load(f)
+
+
 @app.route('/')
 def hello_world():
-    return "Hello, world!"
+    return horoscopes.get('taur') 
