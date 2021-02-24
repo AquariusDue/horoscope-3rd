8ebc256743728ae7e800d773d6a6f0117c90bd48
Mihai Dumitru <mihai14x7@gmail.com>
2021-02-25 01:18:22 +0200

    Rewrite error handling to have better integration with Flask; Return JSON Horoscopes instead of plaintext

--- api/app.py
+++ api/app.py
@@ -1,20 +1,38 @@
-from flask import Flask
+from flask import Flask, jsonify
 import json
 
 app = Flask(__name__)
 
+# Define a custom Exception inherited from KeyError
+class HoroscopeKeyError(KeyError):
+    pass
+
+# Read the whole horoscope data, check if the requested horoscope exists and return only the specific horoscope
 def get_horoscopes(h_sign):
     with open('../horoscopes.json', 'r') as f:
         horoscopes = json.load(f)
-        
-        if h_sign in horoscopes:
-            return horoscopes.get(h_sign)
-        else:
-            return "Error: Not a valid astrological sign"
 
+        if h_sign in horoscopes:
+            return {
+                "sign": h_sign,
+                "text": horoscopes.get(h_sign)
+            }
+        else:
+            raise HoroscopeKeyError("Invalid astrological sign") 
+
+# Flask error handler
+@app.errorhandler(HoroscopeKeyError)
+def handle_invalid_sign(error):
+    response = {
+        "status_code": 400,
+        "error": repr(error),
+        "message": "No such astrological sign",
+        "detail": "Ensure there are no typos or missing letters in the sign name"
+        }
+    return jsonify(response), 400 
+
+# Route to retrieve the requested horoscope based on h_sign e.g. berbec, rac, sagetator
 @app.route('/horoscope/<h_sign>')
 def send_horoscope(h_sign):
     horoscope = get_horoscopes(h_sign)
-    return horoscope
-
-#TODO: Better error-checking, current version is inadequate.
+    return jsonify(horoscope)
